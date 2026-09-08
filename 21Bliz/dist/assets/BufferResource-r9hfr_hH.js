333455ec94834599f48f62791505232e177b9510
Jitendra Chauhan <jitendrachauhan527@gmail.com>
2026-09-08 17:18:26 +0530

    m

--- 21Bliz/dist/assets/BufferResource-r9hfr_hH.js
+++ 21Bliz/dist/assets/BufferResource-r9hfr_hH.js
@@ -1,4 +1,4 @@
-import{w as _,g,c,u as U,B as y,d as l,E as I,e as m}from"./index-DI2kwEnY.js";const h={name:"local-uniform-bit",vertex:{header:`
+import{w as _,g,c,u as U,B as y,d as l,E as I,e as m}from"./index-DGeZl7k_.js";const h={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
