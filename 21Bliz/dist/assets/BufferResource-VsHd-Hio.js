cf6b45479055c1837978c35741f47ca1bdbabe2f
Jitendra Chauhan <jitendrachauhan527@gmail.com>
2026-09-10 17:35:41 +0530

    changes full screen

--- 21Bliz/dist/assets/BufferResource-VsHd-Hio.js
+++ 21Bliz/dist/assets/BufferResource-VsHd-Hio.js
@@ -1,4 +1,4 @@
-import{w as _,g,c,u as U,B as y,d as l,E as I,e as m}from"./index-DPRNb_vg.js";const h={name:"local-uniform-bit",vertex:{header:`
+import{w as _,g,c,u as U,B as y,d as l,E as I,e as m}from"./index-Bp5VZ5CJ.js";const h={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
